234ab139d33741cb4cbe18503508b404e631b347
Rachel Gray <rachel.gray7448@gmail.com>
2022-05-10 19:36:47 -0400

    started login form handler

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -27,4 +27,32 @@ async function signupFormHandler(event) {
             alert(response.statusText);
         }
     }
+};
+
+async function loginFormHandler(event) {
+    event.preventDefault();
+
+    const email = document.querySelector("#").value.trim();
+    const password = document.querySelector("#").value.trim();
+
+    if (email && password) {
+        const response = await fetch("/api/users/login????", {
+            method: "post",
+            body: JSON.stringify({
+                email,
+                password
+            }),
+            headers: {}
+        });
+
+        if (response.ok) {
+            document.location.replace("/??????");
+        }
+        else {
+            alert(response.statusText);
+        }
+    }
 }
+
+document.querySelector("class?").addEventListener("submit", loginFormHandler);
+document.querySelector("class???").addEventListener("submit", signupFormHandler);
