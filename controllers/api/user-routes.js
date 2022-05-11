2734c0b3cd921320b00db4698d17db180d05610c
Kyle Richnafsky <7krich@gmail.com>
2022-05-11 16:48:06 -0400

    log out & verify user id routes added

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -76,13 +76,63 @@ router.post('/', (req, res) => {
         password: req.body.password
     })
     .then(dbUserData => {
-        // save below for when sessions are added
-        // req.session.save(() => {
-        //     req.sessions.user_id = dbUserData.id;
-        //     req.sessions.username = dbUserData.username;
-        //     req.sessions.loggedIn = true;
+         req.session.save(() => {
+             req.sessions.user_id = dbUserData.id;
+             req.sessions.username = dbUserData.username;
+             req.sessions.loggedIn = true;
 
             res.json(dbUserData);
-        // })
+         })
     });
 });
+
+// route to verify user identity
+router.post('/login', (req, res) => {
+    // query User table to find the instance of a user that contains the user's credentials - user's email
+    // expects {email: "", password: ""}
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    })
+    .then(dbUserData => {
+        // if no e-mail is found we don't need to try to verify the password since the acct doesn't exist
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that e-mail address!' });
+            return;
+        }
+        
+        // verify user
+        // pass plaintext pwd stored in req.body.password into checkPassword argument in User.js
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        // if match returns false
+        if(!validPassword) {
+            res.status(400).json({ mesage: 'Incorrect password!' });
+            return;
+        }
+
+        req.session.save(() => {
+            // declare session variables
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+        
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+        });
+    });
+});
+
+// allow user to log out if signed in
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
+});
+
+// DELETE (non-MVP)
+
