2b2a61156165f5d46c9013031a5d98014df7d256
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-05-11 21:02:25 -0400

    update user modeal

--- models/User.js
+++ models/User.js
@@ -18,6 +18,14 @@ User.init(
             primaryKey: true, 
             autoIncrement: true
         },
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        last_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
         username: {
             type: DataTypes.STRING,
             allowNull: false
