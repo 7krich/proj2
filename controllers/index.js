5ed9be5192f2bb69cf5f50b6d7d9873b97540078
evankuck <evankuck1@gmail.com>
2022-05-12 14:29:08 -0400

    updating notes from last nights meeting

---
+++ controllers/index.js
@@ -0,0 +1,15 @@
+<div class="card">
+  <h3 class="card-header text-uppercase">Search By User</h3>
+  <form id="user-form" class="card-body">
+    <label class="form-label" for="username">Username</label>
+    <input name="username" id="username" type="text" autofocus="true" class="form-input" />
+    <button type="submit" class="btn">Get User</button>
+  </form>
+</div>
+
+// NPM package at the top of the website that cycles throug hall of the B words
+//15-20 B Words
+//32 Inspirational B words
+//Look for HTML for Tumblr and add that to your branch as an update
+// Future state is add more tags
+// purple circle potentially opens a new page
