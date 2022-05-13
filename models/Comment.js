994a3eb783c6abbf9451f6b66295d778d8949f3b
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-05-12 20:36:32 -0400

    update post extends model, comment anon

--- models/Comment.js
+++ models/Comment.js
@@ -18,6 +18,10 @@ Comment.init(
                 len: [1]
             }
         },
+        anonymous: {
+            type: DataTypes.BOOLEAN,
+            default: false
+        },
         user_id: {
             type: DataTypes.INTEGER,
             references: {
