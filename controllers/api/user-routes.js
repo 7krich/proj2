592bcb11d18a293247b41c06eb2d63b99ddc2092
Kyle Richnafsky <7krich@gmail.com>
2022-05-14 12:12:32 -0400

    added user id put route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -144,7 +144,29 @@ router.post('/logout', (req, res) => {
     }
 });
 
+router.put('/:id', (req, res) => {
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+  
+    // pass in req.body instead to only update what's passed through
+    User.update(req.body, {
+      individualHooks: true,
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
+
 // DELETE (non-MVP)
-// PUT /api/users/1 (non-MVP)
 
 module.exports = router;
