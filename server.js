826cd8f0a641365874deaf9d6348dc35b169a1d4
a-wiles <98373402+a-wiles@users.noreply.github.com>
2022-05-14 18:10:47 -0400

    Set up foundation files - currently not connected to css and main handlebars

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const path = require('path');
 // set up handlebars.js
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+$(document).foundation()
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -30,6 +32,10 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res) => {
+  res.status(404).render("404page", {title:"404 not found",
+  customstyle: `<link rel="stylesheet" href="/public/src/assets/styles.css">`});
+});
 
 // turn on routes
 app.use(routes);
