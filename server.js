807db9b55643dd93d13e37c010064596085a0e7c
a-wiles <98373402+a-wiles@users.noreply.github.com>
2022-05-15 00:53:25 -0400

    Adjusted li items in navigation, working on linking handlebars and event listenner  next.

--- server.js
+++ server.js
@@ -7,13 +7,14 @@ const path = require('path');
 // set up handlebars.js
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
-$(document).foundation()
+//$(document).foundation()
 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const session = require('express-session');
+const { getEventListeners } = require('events');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
   secret: 'Super secret secret',
