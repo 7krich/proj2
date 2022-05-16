3eeeab3b3915b92d538d60bfe878ac954aa20963
Kyle Richnafsky <7krich@gmail.com>
2022-05-16 19:49:41 -0400

    fixed require file for category-routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,7 +4,7 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
-const categoryRoutes = require('/')
+const categoryRoutes = require('./category-routes');
 
 // add api prefixes
 router.use('/users', userRoutes);
