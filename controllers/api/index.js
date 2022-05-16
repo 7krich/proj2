b12b4c049347b4467bc233ae32f3d17f098778b2
Kyle Richnafsky <7krich@gmail.com>
2022-05-16 17:11:58 -0400

    category routes added

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,10 +4,12 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
+const categoryRoutes = require('/')
 
 // add api prefixes
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
+router.use('/categories', categoryRoutes);
 
 module.exports = router;
