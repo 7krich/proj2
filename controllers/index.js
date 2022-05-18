b93fb76fd0ed0dd9d80a8e816892b7b7ff50c0f4
evankuck <evankuck1@gmail.com>
2022-05-18 12:17:10 -0400

    reminding myself how to commit

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,21 @@
+const generatePage = (name, github) => {
+    return `
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+    </head>
+  
+    <body>
+      <h1>${name}</h1>
+      <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html>
+    `;
+  };
 <div class="card">
   <h3 class="card-header text-uppercase">Search By User</h3>
   <form id="user-form" class="card-body">
@@ -7,9 +25,36 @@
   </form>
 </div>
 
+router.delete('/:id',function(req, res, next){
+  Docket.findById(req.params.id, function(err, docket){
+    docket.remove(function(err){
+      res.redirect('/dockets');
+    });
+  });
+});
+
+<form name="delete-docket-form" id="delete-docket-form" method="delete">
+    <div class="form-submit">
+        <input type="submit" value="Delete my docket" />
+    </div>
+</form>
+
 // NPM package at the top of the website that cycles throug hall of the B words
 //15-20 B Words
 //32 Inspirational B words
 //Look for HTML for Tumblr and add that to your branch as an update
 // Future state is add more tags
 // purple circle potentially opens a new page
+
+// Just make a button in Javascript
+let btn = document.createElement("button");
+btn.innerHTML = "Click Me";
+document.body.appendChild(btn);
+
+// creating rules for the button based on whether it needs to go into a form
+
+let btn = document.createElement("button");
+btn.innerHTML = "Submit";
+btn.type = "submit";
+btn.name = "formBtn";
+document.body.appendChild(btn);
