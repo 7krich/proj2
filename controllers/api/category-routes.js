57364da83f936e6077f0d3584d001892ba1c9fb7
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-05-17 20:44:10 -0400

    update formatting

--- controllers/api/category-routes.js
+++ controllers/api/category-routes.js
@@ -64,6 +64,16 @@ router.get('/:id', (req, res) => {
     });
 });
 
-// POST create category (non-MVP)
+// create a category 
+router.post("/", (req, res) => {
+    Category.create({
+        category_name: req.body.category_name,
+    })
+    .then((dbCategoryData) => res.json(dbCategoryData))
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 module.exports = router;
